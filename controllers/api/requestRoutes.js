f99fffcd3c054ebf5aac7d2ce6ef022fbb04650e
Travis McDermott <tjmcd2010@gmail.com>
2024-07-15 21:30:31 -0700

    Updates all forms and data

--- controllers/api/requestRoutes.js
+++ controllers/api/requestRoutes.js
@@ -1,1 +1,24 @@
-// router.post()
+// Create routes for requests
+const express = require('express');
+const router = express.Router();
+const { PetSittingRequest } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newRequest = await PetSittingRequest.create({
+            ...req.body,
+            owner_id: req.session.owner_id,
+          }
+
+        );
+        res.status(200).json(newRequest);
+    
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+module.exports = router;
+
+
--- public/js/request.js
+++ public/js/request.js
@@ -1,7 +1,16 @@
 document.querySelector("#new-request-form").addEventListener("submit", (event) => {
   event.preventDefault();
   const petName = document.querySelector("#pet-name").value.trim();
-  const requestBody = JSON.stringify({ petName });
+  const petType = document.querySelector("#pet-type").value.trim();
+  const petBreed = document.querySelector("#pet-breed").value.trim();
+  const petWeight = document.querySelector("#pet-weight").value.trim();
+  const serviceType = document.querySelector("#service-type").value.trim();
+  const serviceStartDate = document.querySelector("#service-start-date").value.trim();
+  const serviceEndDate = document.querySelector("#service-end-date").value.trim();
+  const ownerPhone =  document.querySelector("#owner-phone").value.trim();
+
+  const requestBody = JSON.stringify({ petName, petType, petBreed, petWeight, serviceType, serviceStartDate, serviceEndDate, ownerPhone });
+
   fetch("/api/requests", {
     method: "POST",
     body: requestBody,
